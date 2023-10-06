e54f3a62a241717e5fd6f754828d0ceb9a41d501
Your Name <you@example.com>
2023-10-06 10:51:43 +0700

    Add project files.

---
+++ ProExam/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using ProExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ProExamDBEntities5 db = new ProExamDBEntities5();
+
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        // Action to render partial view directly
+        public ActionResult RenderPartialView()
+        {
+            return PartialView("LoginSection");
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginInfo loginInfo)
+        {
+            // Validate loginInfo and perform authentication
+            // For example, you can check against your database
+            var isValidUser = db.LoginInfoes.Any(u => u.UserCode == loginInfo.UserCode && u.Password == loginInfo.Password);
+
+            if (isValidUser)
+            {
+                // Successful login, set user information in session
+                Session["UserCode"] = "hi";
+
+                if (loginInfo.UserCode == "admin")
+                {
+                    return RedirectToAction("ADHomePage", "AdminDashboard");
+                } else 
+                {
+                    Session["UserCode"] = "hi";
+
+                    return RedirectToAction("Home");
+                }
+            }
+            else
+            {
+                // Invalid login, return to the login page with an error message
+                ModelState.AddModelError("", "Invalid student ID or password");
+
+                // Return the loginInfo object to repopulate the form with user-entered values
+                return View("Home", loginInfo);
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            // Clear session
+            Session.Clear();
+
+            // Redirect to the login page
+            return RedirectToAction("Home");
+        }
+
+    }
+}
---
+++ ProExam/Controllers/LoginInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProExam.Models;
+
+namespace ProExam.Controllers
+{
+    public class LoginInfoesController : Controller
+    {
+        private ProExamDBEntities5 db = new ProExamDBEntities5();
+
+        // GET: LoginInfoes
+        public ActionResult LoginInfo()
+        {
+            return View(db.LoginInfoes.ToList());
+        }
+
+        // GET: LoginInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoginInfo loginInfo = db.LoginInfoes.Find(id);
+            if (loginInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loginInfo);
+        }
+
+        // GET: LoginInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoginInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,UserCode,Password,Email,LastLoginDate")] LoginInfo loginInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LoginInfoes.Add(loginInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(loginInfo);
+        }
+
+        // GET: LoginInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoginInfo loginInfo = db.LoginInfoes.Find(id);
+            if (loginInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loginInfo);
+        }
+
+        // POST: LoginInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,UserCode,Password,Email,LastLoginDate")] LoginInfo loginInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(loginInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(loginInfo);
+        }
+
+        // GET: LoginInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoginInfo loginInfo = db.LoginInfoes.Find(id);
+            if (loginInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loginInfo);
+        }
+
+        // POST: LoginInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LoginInfo loginInfo = db.LoginInfoes.Find(id);
+            db.LoginInfoes.Remove(loginInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
